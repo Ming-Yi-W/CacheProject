e9439e6f043cf42dd6393874f31b6561166d14f5
ccxiao5 <734194171@qq.com>
2020-06-02 18:25:26 +0800

    一致性Hash算法

---
+++ Hash/src/main/java/com/chw/CacheManage.java
@@ -0,0 +1,95 @@
+package com.chw;
+import lombok.extern.slf4j.Slf4j;
+import lombok.Data;
+import java.util.*;
+
+@Data
+@Slf4j
+public class CacheManage {
+    private List<CacheNode> cacheNodeList;
+    private long MAX_CIRCLE = (1L << 32) - 1;
+    private Long hash(String nodeName) {
+        return MAX_CIRCLE & nodeName.hashCode();
+    }
+
+    /**
+     * 获取节点所在的下标
+     */
+    private int getNodeIndex(Long hash) {
+        int index = cacheNodeList.size();
+        for (int i = 0; i < cacheNodeList.size(); i++) {
+            if (hash <= hash(cacheNodeList.get(i).getCacheName())) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+    private void printList() {
+        for (CacheNode cacheNode : cacheNodeList) {
+            log.info("cachenode: {}", cacheNode);
+        }
+    }
+    /**
+     * 初始化带有虚拟节点的节点链表
+     */
+    public void initVitualNode(int size, int virtualSize) {
+        cacheNodeList = new ArrayList<>();
+        log.info("mx: {}", MAX_CIRCLE);
+        for (int i = 0; i < size; i++) {
+
+
+            for (int j = 0; j < virtualSize; j++) {
+                CacheNode cacheNode = new CacheNode();
+                cacheNode.setCacheName("" + i + "_" + j + i + j + "_node_" + i + "_" + j);
+                cacheNode.setCacheIP("192.168.1.10" + i);
+                Long hashValue = hash(cacheNode.getCacheName());
+                cacheNode.setHashValue(hashValue);
+                int index = getNodeIndex(hashValue);
+                if (index == cacheNodeList.size()) {
+                    cacheNodeList.add(cacheNode);
+                } else {
+                    cacheNodeList.add(index, cacheNode);
+                }
+            }
+        }
+        printList();
+    }
+
+    /**
+     * 初始化节点列表
+     */
+    public void initCacheNode(int size) {
+        cacheNodeList = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            CacheNode cacheNode = new CacheNode();
+            cacheNode.setCacheName("node_" + i);
+            cacheNode.setCacheIP("192.168.1.10" + i);
+            Long hashValue = hash(cacheNode.getCacheName());
+            cacheNode.setHashValue(hashValue);
+            int index = getNodeIndex(hashValue);
+            if (index == 0) {
+                cacheNodeList.add(cacheNode);
+            } else {
+                cacheNodeList.add(index, cacheNode);
+            }
+
+            printList();
+        }
+
+        printList();
+    }
+
+    /**
+     * 存数据
+     */
+    public void putData(String data) {
+        Long hashValue = hash(data);
+        int index = getNodeIndex(hashValue);
+        if(index == cacheNodeList.size()){
+            index = 0;
+        }
+        log.info("data:{}[{}] put into ====>{}", data, hashValue, cacheNodeList.get(index));
+    }
+}
---
+++ Hash/src/main/java/com/chw/CacheNode.java
@@ -0,0 +1,21 @@
+package com.chw;
+import lombok.Data;
+@Data
+public class CacheNode {
+
+    private String cacheName;
+    private String cacheIP;
+    private Long hashValue;
+    String getCacheName() {
+    	return cacheName;
+    }
+    void setCacheName(String Name) {
+    	cacheName=Name;
+    }
+    void setCacheIP(String IP) {
+    	cacheIP=IP;
+    }
+    void setHashValue(Long value){
+    	hashValue=value;
+    }
+}
